1d8995e01772b145492e08c39a14204679729e6b
gagan0183 <g.deepsingh1@gmail.com>
2017-08-10 01:34:26 +0530

    start qualifiers

--- spring-actions/src/main/java/com/springinaction/autowiring/Cookies.java
+++ spring-actions/src/main/java/com/springinaction/autowiring/Cookies.java
@@ -1,10 +1,12 @@
 package com.springinaction.autowiring;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Primary
 @Component
+@Qualifier("soft")
 public class Cookies implements Dessert {
 
 	@Override
--- spring-actions/src/main/java/com/springinaction/autowiring/Enjoy.java
+++ spring-actions/src/main/java/com/springinaction/autowiring/Enjoy.java
@@ -1,6 +1,7 @@
 package com.springinaction.autowiring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +10,7 @@ public class Enjoy {
 	private Dessert dessert;
 	
 	@Autowired
+	@Qualifier("icecream")
 	public void setDessert(Dessert dessert) {
 		this.dessert = dessert;
 	}
