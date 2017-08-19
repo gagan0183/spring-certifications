72c81cb184469e2f9bbeda5394c0c01ce8fd2231
gagan0183 <g.deepsingh1@gmail.com>
2017-08-19 18:48:49 +0530

    changes test case'

---
+++ SpittersWebAppInitializer/src/test/java/com/comp/app/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package com.comp.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.comp.app.config.RootConfig;
+import com.comp.app.config.SpittersWebAppInitalizer;
+import com.comp.app.config.WebConfig;
+import com.comp.app.controller.HomeController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = { SpittersWebAppInitalizer.class })
+public class HomeControllerTest {
+
+	@Test
+	public void testHomePage() throws Exception {
+		HomeController controller = new HomeController();
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.view().name("home"));
+	}
+}
