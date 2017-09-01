db21a082368c251ce00fe697e4bcca59ae58fe4e
gagan0183 <g.deepsingh1@gmail.com>
2017-09-02 01:09:11 +0530

    comp moving jsp to web-infs

--- EventTracker/src/main/java/com/app/WebConfig.java
+++ EventTracker/src/main/java/com/app/WebConfig.java
@@ -1,12 +1,21 @@
 package com.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.app")
 public class WebConfig {
 
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver v = new InternalResourceViewResolver();
+		v.setPrefix("/WEB-INF/jsp/");
+		v.setSuffix(".jsp");
+		return v;
+	}
 }
