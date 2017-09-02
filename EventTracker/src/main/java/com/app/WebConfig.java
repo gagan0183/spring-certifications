fdda2d5129f067941805a6d0c1705a243e4cfb6e
gagan0183 <g.deepsingh1@gmail.com>
2017-09-03 01:03:23 +0530

    comp multiples

--- EventTracker/src/main/java/com/app/WebConfig.java
+++ EventTracker/src/main/java/com/app/WebConfig.java
@@ -1,18 +1,39 @@
 package com.app;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.app")
 public class WebConfig extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
+		return localeResolver;
+	}
 	
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
@@ -25,5 +46,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/pdfs/**").addResourceLocations("/WEB-INF/pdf/");
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		LocaleChangeInterceptor changes = new LocaleChangeInterceptor();
+		changes.setParamName("language");
+		registry.addInterceptor(changes);
 	}
 }
--- EventTracker/src/main/webapp/WEB-INF/jsp/attendee.jsp
+++ EventTracker/src/main/webapp/WEB-INF/jsp/attendee.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,12 +21,20 @@
 </style>
 </head>
 <body>
+	
+		<a href="?language=en">
+			English
+		</a>
+		<a href="?language=es">
+			Spanish
+		</a>
+
 	<form:form commandName="attendee">
 		<form:errors path="*" cssClass="errorblock" element="div"></form:errors>
-		<label for="textInput1">Enter name:</label>
+		<label for="textInput1"><spring:message code="attendee.name"/></label>
 		<form:input path="name" cssErrorClass="error"/>
 		<form:errors path="name" cssClass="error"></form:errors>
-		<label for="textInput2">Enter name:</label>
+		<label for="textInput2"><spring:message code="attendee.email.address"/></label>
 		<form:input path="emailAddress" cssErrorClass="error"/>
 		<form:errors path="emailAddress" cssClass="error"></form:errors>
 		<br/>
